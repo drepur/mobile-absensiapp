39a7021396149c39011f003aab79a5d58a5080d0
Ubuntu <ubuntu@ip-172-31-10-197.ec2.internal>
2021-01-16 08:56:20 +0000

    absensi-mobile

---
+++ src/views/KelasDetail.vue
@@ -0,0 +1,63 @@
+<template>
+    <ion-page>
+        <ion-toolbar color="success">
+            <ion-title>Detail Kelas</ion-title>
+        </ion-toolbar>
+
+        <ion-content>
+            <ion-list>
+                <ion-item>
+                    <ion-label>Pertemuan 1</ion-label>
+                    <ion-badge color="success" slot="end">Hadir</ion-badge>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Pertemuan 2</ion-label>
+                    <ion-badge color="success" slot="end">Hadir</ion-badge>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Pertemuan 3</ion-label>
+                    <ion-badge color="success" slot="end">Hadir</ion-badge>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Pertemuan 4</ion-label>
+                    <ion-badge color="success" slot="end">Hadir</ion-badge>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Pertemuan 5</ion-label>
+                    <ion-badge color="success" slot="end">Hadir</ion-badge>
+                </ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+    import {
+        IonPage,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonList,
+        IonItem,
+        IonLabel,
+        IonBadge
+    } from '@ionic/vue'
+    import { defineComponent } from 'vue'
+
+    export default  defineComponent({
+      components: {
+          IonPage,
+          IonToolbar,
+          IonTitle,
+          IonContent,
+          IonList,
+          IonItem,
+          IonLabel,
+          IonBadge
+      }  
+    })
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar color="success">
+                <ion-title>Login</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+            <ion-item>
+                <ion-label position="floating">Email</ion-label>
+                <ion-input></ion-input>
+            </ion-item>
+            <ion-item>
+                <ion-label position="floating">Password</ion-label>
+                <ion-input></ion-input>
+            </ion-item>
+            <ion-button expand="block" color="danger">Login</ion-button>
+        </ion-content>
+
+        <ion-footer></ion-footer>    
+    </ion-page>    
+</template>
+
+<script>
+import {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonContent,
+    IonFooter,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton
+} from '@ionic/vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    components: {
+        IonPage,
+        IonHeader,
+        IonTitle,
+        IonToolbar,
+        IonContent,
+        IonFooter,
+        IonItem,
+        IonLabel,
+        IonInput,
+        IonButton
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
